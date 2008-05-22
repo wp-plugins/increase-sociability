af948147289e00e9e67f950efac9248c0f65b656
preblogging <preblogging@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2008-05-22 12:06:38 +0000

    first additon of the plugin itself
    git-svn-id: https://plugins.svn.wordpress.org/increase-sociability/trunk@47357 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ increase-sociability.php
@@ -0,0 +1,153 @@
+<?php
+
+/*
+Plugin Name: Increase Sociability
+Plugin URI: http://www.preblogging.com/increase-sociability/
+Description: This is a call to action to increase to get your StumbleUpon & Digg visitors to vote for your site. This is based on  heavily on <a href="http://www.richardkmiller.com/blog/wordpress-plugin-what-would-seth-godin-do/">What Would Seth Godin Do</a> by <a href="http://www.richardkmiller.com/blog/">Richard K Miller</a>. 
+Version: 1.1.0
+Author: Becky Sanders
+Author URI: http://www.preblogging.com
+
+*/
+
+$defaultdata = array(
+	'new_digg_visitor_message' => "<p style='border:thin dotted black; padding:3mm;'>Hello fellow <b>Digger</b>, if you like this page, don't forget to digg it ! </p>",
+	'new_stumble_visitor_message' => "<div style='border:thin dotted black; padding:3mm;'>Welcome, <b>StumbleUpon</b> visitor! Don't forget to give me thumbs up if you like this page !</div>",
+	'message_location' => 'before_post',
+	'nice_link' => 'no'
+	);
+	
+add_option('incSoc_settings', $defaultdata, 'Options for Increasing StumbleUpon/Digg Traffic');
+
+$incSoc_settings = get_option('incSoc_settings');
+$incSoc_settings['new_digg_visitor_message'] = stripslashes($incSoc_settings['new_digg_visitor_message']);
+$incSoc_settings['new_stumble_visitor_message'] = stripslashes($incSoc_settings['new_stumble_visitor_message']);
+
+add_action('admin_menu', 'add_incSoc_options_page');
+
+
+add_filter('the_content', 'incSoc_message_filter');
+
+function add_incSoc_options_page()
+{
+	if (function_exists('add_options_page'))
+	{
+		add_options_page('Increase Social Media Traffic', 'Increase Sociability', 8, basename(__FILE__), 'incSoc_options_subpanel');
+	}
+}
+
+
+
+function incSoc_options_subpanel()
+{
+	global $incSoc_settings, $_POST;
+	
+	if (isset($_POST['submit']))
+	{
+		$incSoc_settings['new_digg_visitor_message'] = stripslashes($_POST['new_digg_visitor_message']);
+		$incSoc_settings['new_stumble_visitor_message'] = stripslashes($_POST['new_stumble_visitor_message']);
+		$incSoc_settings['message_location'] = $_POST['message_location'];
+		$incSoc_settings['nice_link'] = $_POST['nice_link'];
+		update_option('incSoc_settings', $incSoc_settings);
+	}
+	?>
+	<div class="wrap">
+        <h2>Increase StumbleUpon and Digg votes</h2>
+        <p>When the browser detects that a visitor is coming from StumbleUpon.com or from Digg.com, it will give them a little reminder that then should give you a vote. This plugin should help increase the precentage of visitors to voters a little (or a lot).</p>
+        
+        <form action="" method="post">
+        <h3>Message to Visitors from Digg.com:</h3>
+        <textarea rows="4" cols="80" name="new_digg_visitor_message"><?php echo htmlentities($incSoc_settings['new_digg_visitor_message']); ?></textarea>
+        <h3>Message to  Visitors from StumbleUpon.com:</h3>
+        <textarea rows="6" cols="80" name="new_stumble_visitor_message"><?php echo htmlentities($incSoc_settings['new_stumble_visitor_message']); ?></textarea>
+        <h3>Location of Message</h3>
+        <p><input type="radio" name="message_location" value="before_post" <?php if ($incSoc_settings['message_location'] == 'before_post') echo 'checked="checked"'; ?> /> Before Post</p>
+        <p><input type="radio" name="message_location" value="after_post" <?php if ($incSoc_settings['message_location'] == 'after_post') echo 'checked="checked"'; ?> /> After Post</p>
+         <p><input type="radio" name="message_location" value="both_post" <?php if ($incSoc_settings['message_location'] == 'both_post') echo 'checked="checked"'; ?> /> Both Before & After Post</p>
+        <h3>Add link in the footer to Plugin Author</h3>
+	<p>If you would like to add a link back to the author (<a href="http://www.preblogging.com/">www.preblogging.com</a>) you can do so by selecting yes. Of course, like everything in life, you have a choice <img src="<?php echo get_option('home') . "/wp-includes/images/smilies/icon_biggrin.gif"	?>" title="wink wink">. The link will be in the footer and will say the following<br/>
+	<div style="border:thin dotted black; padding:3mm;" >I'm happy to use <a href="http://www.preblogging.com/increase-sociability/" title="Increase Stumbleupon & Digg Traffic">Increase Sociability</a>.</div>
+	</p>	
+
+		 <p><input type="radio" name="nice_link" value="yes" <?php if ($incSoc_settings['nice_link'] == 'yes') echo 'checked="checked"'; ?> /> yeah sure, why not !</p>
+        <p><input type="radio" name="nice_link" value="no" <?php if ($incSoc_settings['nice_link'] == 'no') echo 'checked="checked"'; ?> /> no thanks, not right now</p>
+				<p><input type="submit" name="submit" value="Save Settings" /></p>
+        </form>
+        </div>
+	<?php
+}
+
+
+
+function incSoc_message_filter($content = '')
+{
+	global $incSoc_visits, $incSoc_settings, $incSoc_messagedisplayed;
+
+	$returnValue = $content;
+	
+	if ( !is_feed() && !$incSoc_messagedisplayed )
+	{
+		$incSoc_messagedisplayed = true;
+		$browser_message = '';
+		
+		if($_SERVER['HTTP_REFERER'] != ''){
+			$URL = parse_url($_SERVER['HTTP_REFERER']);
+		
+		 if ($URL['host'] =="www.stumbleupon.com"){
+
+			$browser_message = $incSoc_settings['new_stumble_visitor_message'] ;
+			}
+		else if ($URL['host'] =="digg.com" || $URL['host'] =="www.digg.com"){
+			$browser_message = $incSoc_settings['new_digg_visitor_message'] ;
+			}	
+		}
+		
+
+		if ($incSoc_settings['message_location'] == 'before_post')
+		{
+			$returnValue =
+				$browser_message
+				.
+				$content
+			;
+		}
+		else if ($incSoc_settings['message_location'] == 'after_post')
+		{
+			$returnValue =
+				$content
+				.
+				$browser_message
+				.
+				$incSoc_settings['new_visitor_message']
+			;
+		}
+		else {
+				$returnValue =
+				$browser_message
+				.
+				$content
+				.
+				$browser_message
+			;
+		}
+	}
+//$returnValue .= "<br/>testdata : " . $URL['host'] . "<br/>" ;  
+	return $returnValue;
+}
+
+function preblogging_notice() {
+global  $incSoc_settings;
+$comment =  "\n<!-- Increase Socialability 1.1 - http://www.preblogging.com/increase-sociability/ -->\n " ;
+	if ($incSoc_settings['nice_link'] == 'yes'){
+		echo '<p>I\'m happy to use <a href="http://www.preblogging.com/increase-sociability/" title="Increase Stumbleupon & Digg Traffic">Increase Sociability</a>.</p>';
+		echo $comment;
+	}else {
+		echo $comment;
+	}
+}
+
+
+	
+add_action( 'wp_footer', 'preblogging_notice' );
+
+?>
--- readme.txt
+++ readme.txt
@@ -14,57 +14,27 @@ The best way to increase your traffic from Digg.com and StumbleUpon.com is to ge
 By providing a call to action 
 == Installation ==
 
-This section describes how to install the plugin and get it working.
+How to install the plugin
 
 e.g.
 
 1. Upload `increase-sociability.php` to the `/wp-content/plugins/` directory
-1. Activate the plugin through the 'Plugins' menu in WordPress
-
+2. Go to your WordPress control panel, and visit the Plugin tab.
+3. Enable the plugin called “Increased Sociability“
+4. Go to the Options tab, and select the sub tab “Increased Sociability”
+5. Here you can tweak the plugin, but there enough data present to get you started.
 
 == Frequently Asked Questions ==
 
-= A question that someone might have =
+= How can I check if the plugin is working  =
 
-An answer to that question.
+The easiest way is to visit any blog post and search in the page source for the words '<!-- Increase Socialability ', this should be towards the bottom of the page
 
-= What about foo bar? =
+= Is this illegal=
 
-Answer to foo bar dilemma.
+Nope, all this plugin does is to remind people to vote for you if they like your blog post.
 
 == Screenshots ==
 
-1. This screen shot description corresponds to screenshot-1.(png|jpg|jpeg|gif). Note that the screenshot is taken from
-the directory of the stable readme.txt, so in this case, `/tags/4.3/screenshot-1.png` (or jpg, jpeg, gif)
-2. This is the second screen shot
+1. no screen shots necessary as it's really straight forward !
 
-== Arbitrary section ==
-
-You may provide arbitrary sections, in the same format as the ones above.  This may be of use for extremely complicated
-plugins where more information needs to be conveyed that doesn't fit into the categories of "description" or
-"installation."  Arbitrary sections will be shown below the built-in sections outlined above.
-
-== A brief Markdown Example ==
-
-Ordered list:
-
-1. Some feature
-1. Another feature
-1. Something else about the plugin
-
-Unordered list:
-
-* something
-* something else
-* third thing
-
-Here's a link to [WordPress](http://wordpress.org/ "Your favorite software") and one to [Markdown's Syntax Documentation][markdown syntax].
-Titles are optional, naturally.
-
-[markdown syntax]: http://daringfireball.net/projects/markdown/syntax
-            "Markdown is what the parser uses to process much of the readme file"
-
-Markdown uses email style notation for blockquotes and I've been told:
-> Asterisks for *emphasis*. Double it up  for **strong**.
-
-`<?php code(); // goes in backticks ?>`
